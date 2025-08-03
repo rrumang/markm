1678caec5a88358659335401bf9921cfff9c2b6e
rrumang <rrumang@gmail.com>
2025-08-03 12:54:45 +0900

    메인페이지 - 배너이미지 수정

--- src/main/java/jpabarcode/jpashop/controller/HomeController.java
+++ src/main/java/jpabarcode/jpashop/controller/HomeController.java
@@ -2,7 +2,6 @@ package jpabarcode.jpashop.controller;
 
 import jpabarcode.jpashop.domain.Board;
 import jpabarcode.jpashop.domain.Member;
-import jpabarcode.jpashop.repository.MemberRepository;
 import jpabarcode.jpashop.service.BoardService;
 import jpabarcode.jpashop.service.LoginService;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +27,6 @@ public class HomeController {
 
     @RequestMapping("/")
     public String homeV3(@SessionAttribute(name = "LOGIN_MEMBER", required = false) Member loginMember, Model model ) {
-        log.info("home controller");
         //세션에 회원데이터가 없으면 home
         model.addAttribute("member", Objects.requireNonNullElseGet(loginMember, Member::new));
 
@@ -36,15 +34,20 @@ public class HomeController {
         List<Board> banners = boardService.findByType("banner");
         model.addAttribute("banners", banners);
 
+        // 공지사항 5개불러오기
+        List<Board> notices = boardService.findByTypeLimit("notice", 5);
+        model.addAttribute("notices", notices);
+
         return "home";
     }
 
-    //관리자 기능
+    // 로그인화면 이동
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("form") LoginForm form) {
         return "admin/signin";
     }
 
+    // 로그인
     @PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute("form") LoginForm form, BindingResult result, HttpServletRequest request) {
         if (result.hasErrors()) return "admin/signin";
@@ -64,6 +67,7 @@ public class HomeController {
         return "redirect:/";
     }
 
+    // 로그아웃
     @PostMapping("/logout")
     public String logoutV3(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
@@ -73,25 +77,29 @@ public class HomeController {
         return "redirect:/";
     }
 
-    // 회사소개
+    // 회사소개 - CEO 인사말
     @RequestMapping("/company/about")
     public String about(@SessionAttribute(name = "LOGIN_MEMBER", required = false) Member loginMember, Model model) {
-        log.info("company controller");
+        log.info("about controller");
 
         //세션 회원데이터
         model.addAttribute("member", Objects.requireNonNullElseGet(loginMember, Member::new));
 
+        Board about = boardService.findByType("about").get(0);
+        model.addAttribute("about", about);
         return "company/about";
     }
 
-    // 찾아오시는길
+    // 회사소개 - 찾아오시는길
     @RequestMapping("/company/about2")
     public String about2(@SessionAttribute(name = "LOGIN_MEMBER", required = false) Member loginMember, Model model) {
-        log.info("company controller");
+        log.info("about2 controller");
 
         //세션 회원데이터
         model.addAttribute("member", Objects.requireNonNullElseGet(loginMember, Member::new));
 
+        Board about2 = boardService.findByType("about2").get(0);
+        model.addAttribute("about2", about2);
         return "company/about2";
     }
 
